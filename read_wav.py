f0087623528f5880bd480fc490bf11c73772402e
MHuang2001 <mhuang2001@hotmail.com>
2022-04-27 14:31:05 +1000

    Notebook

--- read_wav.py
+++ read_wav.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import wave
-import sys
 
 
-spf = wave.open("/Users/martinhuang/Desktop/OneDrive/2022/2022 Sem 1/DATA3888/Brain 5/Brain/Spiker_box_Louis/Short/LLL_L1.wav", "r")
+def plot_wave(path):
+    path = str(path)
+    spf = wave.open(path, "r")
+    signal = spf.readframes(-1)
+    signal = np.fromstring(signal, np.int16)
+    fs = spf.getframerate()
 
-signal = spf.readframes(-1)
-signal = np.fromstring(signal, np.int16)
-fs = spf.getframerate()
+    Time = np.linspace(0, len(signal) / fs, num=len(signal))
 
-Time = np.linspace(0, len(signal) / fs, num=len(signal))
+    plt.figure(1)
+    plt.title("Signal Wave...")
+    plt.plot(Time, signal)
+    plt.show()
 
-plt.figure(1)
-plt.title("Signal Wave...")
-plt.plot(Time, signal)
-plt.show()
-
-
+plot_wave("/Users/martinhuang/Desktop/OneDrive/2022/2022 Sem 1/DATA3888/Brain 5/Brain/arm_signal/EEU.wav")
+plot_wave("/Users/martinhuang/Desktop/OneDrive/2022/2022 Sem 1/DATA3888/Brain 5/Brain/arm_signal/UUU.wav")
 
